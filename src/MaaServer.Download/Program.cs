d33935dbb09a34df64a16056a67acbfaffbdeac5
Liam Sho <conan_creeper@outlook.com>
2022-01-13 16:28:32 +0800

    init.下载服务器项目初始化

---
+++ src/MaaServer.Download/Program.cs
@@ -0,0 +1,26 @@
+using Serilog;
+
+#region Build configuration and logger
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile("appsettings.Development.json", optional: true)
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .Build();
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(configuration)
+    .CreateLogger();
+
+Log.Logger.Information("启动中...");
+
+#endregion
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSerilog();
+
+var app = builder.Build();
+
+app.MapControllers();
+app.Run();
