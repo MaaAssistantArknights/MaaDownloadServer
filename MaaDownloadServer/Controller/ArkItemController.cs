7375a240d7c4438e65c1e9d9e7d45663f09dc143
Liam Sho <conan_creeper@outlook.com>
2022-02-27 12:52:32 +0800

    feat.添加 GameData Controller

---
+++ MaaDownloadServer/Controller/ArkItemController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MaaDownloadServer.Controller;
+
+[ApiController]
+[Route("game-data/item")]
+public class ArkItemController : ControllerBase
+{
+    private readonly IArkItemService _arkItemService;
+
+    public ArkItemController(IArkItemService arkItemService)
+    {
+        _arkItemService = arkItemService;
+    }
+
+    [HttpGet("{name}")]
+    public async Task<ActionResult<GetItemDto>> GetArkItem(string name)
+    {
+        var dto = await _arkItemService.GetItem(name);
+
+        if (dto is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(dto);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<QueryItemsDto>> QueryArkItems(
+        [FromQuery] string name,
+        [FromQuery] string limit,
+        [FromQuery] string page)
+    {
+        var realLimit = 50;
+        var realPage = 1;
+
+        if (int.TryParse(limit, out var parsedLimit))
+        {
+            if (parsedLimit >= 0)
+            {
+                realLimit = parsedLimit;
+            }
+        }
+
+        if (int.TryParse(page, out var parsedPage))
+        {
+            if (parsedPage >= 0)
+            {
+                realPage = parsedPage;
+            }
+        }
+
+        var dto = await _arkItemService.QueryItems(name, realLimit, realPage);
+        return Ok(dto);
+    }
+}
---
+++ MaaDownloadServer/Controller/ArkStageController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Immutable;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaaDownloadServer.Controller;
+
+[ApiController]
+[Route("game-data/stage")]
+public class ArkStageController : ControllerBase
+{
+    private readonly IArkStageService _arkStageService;
+
+    public ArkStageController(IArkStageService arkStageService)
+    {
+        _arkStageService = arkStageService;
+    }
+
+    [HttpGet("{stage_code}")]
+    // ReSharper disable once InconsistentNaming
+    public async Task<ActionResult<GetStageDto>> GetArkStage(string stage_code)
+    {
+        var dto = await _arkStageService.GetStage(stage_code);
+
+        if (dto is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(dto);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<QueryStagesDto>> QueryArkStages(
+        // ReSharper disable InconsistentNaming
+        [FromQuery] string stage_id,
+        [FromQuery] string stage_type,
+        [FromQuery] string stage_code,
+        [FromQuery] string ap_cost_lower_limit,
+        [FromQuery] string ap_cost_up_limit,
+        [FromQuery] string zone_id,
+        [FromQuery] string zone_name,
+        [FromQuery] string zone_type,
+        [FromQuery] string available_cn,
+        [FromQuery] string available_kr,
+        [FromQuery] string available_us,
+        [FromQuery] string available_jp,
+        [FromQuery] string limit,
+        [FromQuery] string page)
+    // ReSharper restore InconsistentNaming
+    {
+        var allQueries = new Dictionary<string, string>
+        {
+            { "page", page },
+            { "limit", limit },
+            { "stage_id", stage_id },
+            { "stage_type", stage_type },
+            { "stage_code", stage_code },
+            { "ap_cost_lower_limit", ap_cost_lower_limit },
+            { "ap_cost_up_limit", ap_cost_up_limit },
+            { "zone_id", zone_id },
+            { "zone_name", zone_name },
+            { "zone_type", zone_type },
+            { "available_cn", available_cn },
+            { "available_kr", available_kr },
+            { "available_us", available_us },
+            { "available_jp", available_jp }
+        };
+
+        var queries = allQueries.Where(x => x.Value is not "" && x.Value is not null).ToImmutableDictionary();
+
+        var dto = await _arkStageService.QueryStages(queries);
+
+        return Ok(dto);
+    }
+}
