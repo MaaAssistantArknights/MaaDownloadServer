9f8e5d733a79ba6d132004358fe8e93c7cfe46a4
Liam Sho <conan_creeper@outlook.com>
2022-03-19 18:32:22 +0800

    chore.移除无用代码

--- MaaDownloadServer/Model/External/Api/ApiPenguinDropInfos.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace MaaDownloadServer.Model.External;
-
-public record ApiPenguinDropInfos
-{
-    [JsonPropertyName("itemId")] public string ItemId { get; set; }
-    [JsonExtensionData] public Dictionary<string, object> ExtensionData { get; set; }
-}
